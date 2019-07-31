f624c7937f393c183e1796608a4f3be792b270d8
hegaoye <hegaoye@qq.com>
2019-07-31 11:44:57 +0800

    提交稳定代码

--- src/service/alipay.py
+++ src/service/alipay.py
@@ -147,10 +147,10 @@ class AliPay:
         time.sleep(.5)
         notify_count = self.alipayxmldata.notify_list(device_id)
         if notify_count > 0:
-            # TODO  清理通知
-            # if not self.debug:
-            #     x, y = self.alipayxmldata.get_click_clear_notify_x_y(self.device_id)
-            #     self.click(x, y)
+            # 清理通知
+            if not self.debug:
+                x, y = self.alipayxmldata.get_click_clear_notify_x_y(self.device_id)
+                self.click(x, y)
 
             self.back_to_desktop()
             time.sleep(.1)
